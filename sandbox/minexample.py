d30928317fd8be5b9503762727ff56faa25772eb
Hans Fangohr iota <fangohr@soton.ac.uk>
2017-06-22 14:26:57 +0100

    intermediate commit - start to refactor (with Leoni)

--- sandbox/minexample.py
+++ sandbox/minexample.py
@@ -1,8 +1,21 @@
 # coding: utf-8
 
-import fenics as df
+
+import scipy.integrate
+import fenics as fe
 import numpy as np
 import matplotlib.pyplot as plt
+
+def setup_vectorspace(mesh):
+    """setup"""
+    V = fe.VectorFunctionSpace(mesh, "CG", 1, dim=3)
+    v = fe.TestFunction(V)
+    u = fe.TrialFunction(V)
+    m = fe.Function(V)
+    Heff = fe.Function(V)
+    return m, Heff, u, v, V
+
+
 
 # Material parameters
 Ms = 8.6e5  # saturation magnetisation (A/m)
@@ -18,11 +31,9 @@ B = 0.1  # (T)
 mu0 = 4 * np.pi * 1e-7  # vacuum permeability
 
 # Zeeman field
-H = Ms / 2 * df.Constant((0,0,1))
+H = Ms / 2 * fe.Constant((0,0,1))
 # easy axis
-ea = df.Constant((0,1,1))
-# meaningful time period is of order of nano seconds
-dt = 1e-12
+ea = fe.Constant((0,1,1))
 
 # mesh parameters
 d = 100e-9
@@ -31,51 +42,43 @@ nx = ny = 20
 nz = 10
 
 # create mesh
-p1 = df.Point(0, 0, 0)
-p2 = df.Point(d, d, thickness)
-mesh = df.BoxMesh(p1, p2, nx, ny, nz)
+p1 = fe.Point(0, 0, 0)
+p2 = fe.Point(d, d, thickness)
+mesh = fe.BoxMesh(p1, p2, nx, ny, nz)
 
-# define function space for magnetization
-V = df.VectorFunctionSpace(mesh, "CG", 1, dim=3)
-v = df.TestFunction(V)
-u = df.TrialFunction(V)
 
-# define initial M and normalise
-m = df.Function(V)
+m, Heff, u, v, V = setup_vectorspace(mesh)
 
 def effective_field(m, volume=None):
-    w_Zeeman = - mu0 * Ms * df.dot(m, H)
-    w_exchange = A  * df.inner(df.grad(m), df.grad(m))
-    w_DMI = D * df.inner(m, df.curl(m))
-    w_ani = - K * df.inner(m, ea)**2
+    w_Zeeman = - mu0 * Ms * fe.dot(m, H)
+    w_exchange = A  * fe.inner(fe.grad(m), fe.grad(m))
+    w_DMI = D * fe.inner(m, fe.curl(m))
+    w_ani = - K * fe.inner(m, ea)**2
     w = w_Zeeman + w_exchange + w_DMI + w_ani
-    return -1/(mu0*Ms) * df.derivative(w*df.dx, m)
+    return -1/(mu0*Ms) * fe.derivative(w*fe.dx, m)
 
 # Effective field
 Heff_form = effective_field(m)
 
-
-# In[15]:
-
 # Preassemble projection Matrix
-Amat = df.assemble(df.dot(u, v)*df.dx)
+Amat = fe.assemble(fe.dot(u, v)*fe.dx)
 
-LU = df.LUSolver()
+LU = fe.LUSolver()
 LU.set_operator(Amat)
-Heff = df.Function(V)
+
 
 def compute_dmdt(m):
     """Convenience function that does all in one go"""
 
     # Assemble RHS
-    b = df.assemble(Heff_form)
+    b = fe.assemble(Heff_form)
 
     # Project onto Heff
     LU.solve(Heff.vector(), b)
 
-    LLG = -gamma/(1+alpha*alpha)*df.cross(m, Heff) - alpha*gamma/(1+alpha*alpha)*df.cross(m, df.cross(m, Heff))
+    LLG = -gamma/(1+alpha*alpha)*fe.cross(m, Heff) - alpha*gamma/(1+alpha*alpha)*fe.cross(m, fe.cross(m, Heff))
 
-    result = df.assemble(df.dot(LLG, v)*df.dP)
+    result = fe.assemble(fe.dot(LLG, v)*fe.dP)
     return result.array()
 
 # function for integration of system of ODEs
@@ -87,27 +90,14 @@ def rhs_micromagnetic(m_vector_array, t, counter=[0]):
     dmdt = compute_dmdt(m)
     return dmdt
 
-
-# ## Integrate ODEs with scipy.integrate
-
-# In[16]:
-
-from scipy.integrate import odeint
-m_init = df.Constant((1, 0, 0))
-m = df.interpolate(m_init, V)
+m_init = fe.Constant((1, 0, 0))
+m = fe.interpolate(m_init, V)
 ts = np.linspace(0, 5e-10, 100)
 # empty call of time integrator, just to get FEniCS to cache all forms etc
 rhs_micromagnetic(m.vector().array(), 0)
 
+ms = scipy.integrate.odeint(rhs_micromagnetic, y0=m.vector().array(), t=ts, rtol=1e-10, atol=1e-10)
 
-# In[17]:
-
-ms = odeint(rhs_micromagnetic, y0=m.vector().array(), t=ts, rtol=1e-10, atol=1e-10)
-
-
-# In[18]:
-
-import pylab
 
 def macrospin_analytic_solution(alpha, gamma, H, t_array):
     """
@@ -120,7 +110,8 @@ def macrospin_analytic_solution(alpha, gamma, H, t_array):
     Appendix B, page 127
 
     """
-    t0 = 1 / (gamma * alpha * H) *         np.log(np.sin(np.pi / 2) / (1 + np.cos(np.pi / 2)))
+    t0 = 1 / (gamma * alpha * H) * np.log(np.sin(np.pi / 2) / \
+                                          (1 + np.cos(np.pi / 2)))
     mx_analytic = []
     for t in t_array:
         phi = gamma * H * t                                     # (B16)
@@ -133,29 +124,21 @@ def macrospin_analytic_solution(alpha, gamma, H, t_array):
 mx_analytic = macrospin_analytic_solution(alpha, gamma, Ms/2, ts)
 
 tmp2 = ms[:,0:1]  # might be m_x, m_y, m_z of first vector
-pylab.plot(ts, tmp2, 'o-')
-pylab.plot(ts, mx_analytic, 'x-')
-pylab.legend(['simulation', 'analytical'])
-pylab.xlabel('$t\,$[s]')
-pylab.ylabel('$<M_x>$')
-
-
-# In[19]:
+# pylab.plot(ts, tmp2, 'o-')
+# pylab.plot(ts, mx_analytic, 'x-')
+# pylab.legend(['simulation', 'analytical'])
+# pylab.xlabel('$t\,$[s]')
+# pylab.ylabel('$<M_x>$')
 
 difference = tmp2[:,0] - mx_analytic
 print("max deviation: {}".format(max(abs(difference))))
-pylab.plot(ts, difference, 'o-')
-pylab.legend(['simulation - analytical'])
-pylab.xlabel('$t\,$[s]')
-pylab.ylabel('$<\Delta M_x>$')
+# pylab.plot(ts, difference, 'o-')
+# pylab.legend(['simulation - analytical'])
+# pylab.xlabel('$t\,$[s]')
+# pylab.ylabel('$<\Delta M_x>$')
 
-
-# In[21]:
 
 # add quick test
 eps = 0.01
 ref = 0.0655908475021
 assert ref - eps < max(abs(difference)) < ref + eps
-
-
-# In[ ]:
